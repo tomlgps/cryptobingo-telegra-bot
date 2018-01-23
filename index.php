33cb27dbb2a67025b6e6194d2ac7b45242ed7991
MT <nikto1310@gmail.com>
2018-01-23 18:31:25 +0300

    attempt two

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
 
-
 require 'vendor/autoload.php';
 
 use Telegram\Bot\Api;
 use App\Controllers\{CommandController, UpdatesController};
 
-
 $token = '421975949:AAHBU7Iyts-4HNZ2dOHgU2Fcm1MHH1I9bAY';
 
 $telegram = new Api($token);
-
 
 $updates_controller = new UpdatesController(
     $telegram,
